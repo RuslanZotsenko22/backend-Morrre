f3894f9d6da3dda72cfc176ff22f760ff749085c
Ruslan <justmymail989@gmail.com>
2025-10-20 18:59:45 +0200

    feat(auth): add email OTP registration flow with Redis & Mailtrap SMTP

---
+++ src/auth/dto/verify-code.dto.ts
@@ -0,0 +1,5 @@
+import { IsEmail, IsString, Length } from 'class-validator';
+export class VerifyCodeDto {
+  @IsEmail() email: string;
+  @IsString() @Length(6, 6) code: string; // 6-значний код
+}
